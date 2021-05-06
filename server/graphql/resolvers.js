1f9ae43f6bb6dae272580a1495e261231798e824
Brian Chiang <chiangbri@gmail.com>
2021-05-06 09:09:19 -0700

    comments

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -10,10 +10,11 @@ export const resolvers = {
       const myJson = await csv()
         .fromFile(path.join(__dirname, '../db/redfin.csv'))
         .then((response) => {
-          // Parse the response so the keys come back without spaces. We are just ignoring the keys with special characters for now and ommiting them from the type definition for a home
+          // Mutate the response so the keys come back without spaces to match the graphql spec. I'm ignoring the keys with special characters for now and omitting them from the type definition for a home listing. )
           return response.map((response) => {
             const formattedResponse = {};
             const keys = Object.keys(response).map((each) =>
+              // alternatively, we could use regex to replace spaces and special characters. We'd also need a conditional to parse the url key since it would still be a nonsense key even with the spaces and special characters removed. Maybe something like if(each.indexOf('URL) !== -1) return 'URL'
               each.replace(' ', '')
             );
             const values = Object.values(response);
